25e3870384a4a95e1355932477d46ad53e500127
1234stpcrng9876 <timklep.maps9270@gmail.com>
2026-05-03 15:46:34 +0300

    Добавление на гитхаб

---
+++ cards.js
@@ -0,0 +1,18 @@
+// Базовый набор карточек, который загружается по умолчанию
+const defaultCards = [
+    {
+        "word": "Welcome",
+        "translation": "Добро пожаловать",
+        "example": "Welcome to your new flashcard app!"
+    },
+    {
+        "word": "Vocabulary",
+        "translation": "Словарный запас",
+        "example": "Reading helps you build your vocabulary."
+    },
+    {
+        "word": "Consistent",
+        "translation": "Последовательный, постоянный",
+        "example": "You need to be consistent to see results."
+    }
+];
